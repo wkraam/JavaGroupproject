958a45c847146fc0e6f2544411e476f4ddb6c819
Walther Kraam <wkraam@yahoo.com>
2021-03-24 22:03:30 +0200

    added simple GUI

--- Rühmatöö/src/MängijaComputerHard.java
+++ Rühmatöö/src/MängijaComputerHard.java
@@ -1,10 +1,10 @@
-public class MängijaComputerHard extends MängijaComputer{
+public class MängijaComputerHard extends MängijaComputer {
     @Override
     public int käik(int tikkudeArv) {
         //võidab kui
-        int b = (tikkudeArv-1)%4;
-        if (b==0){
-            b=1;
+        int b = (tikkudeArv - 1) % 4;
+        if (b == 0) {
+            b = 1;
         }
         return b;
     }
