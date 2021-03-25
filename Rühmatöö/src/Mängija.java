ffe554d81dc65d22b892faed0b27ce3e2d261ee7
Walther Kraam <wkraam@yahoo.com>
2021-03-25 17:05:03 +0200

    comments
    kommentaarid

--- Rühmatöö/src/Mängija.java
+++ Rühmatöö/src/Mängija.java
@@ -1,30 +1,33 @@
 import javax.swing.*;
 
 public class Mängija {
-    private String nimi;
+    private String nimi; // nime hoidmise väli
 
     public Mängija(String nimi) {
         this.nimi = nimi;
-    }
+    } // konstruktor
 
     public Mängija() {
     }
 
     public String getNimi() {
         return nimi;
-    }
+    } // nime tagastamise get meetod
 
-    public int käik(int tikkudeArv) {
-        int tikud;
+    public int käik(int tikkudeArv) { // meetod, kus tehakse tikkude valik ning see tagastatakse
+        int tikud; // väli tikkude valiku kohta
         while (true) {
-            Object[] optionsVastane = {"1", "2", "3"};
-            tikud = (JOptionPane.showOptionDialog(null, nimi + ": vali, mitu tikku võtad:", "tikkude valimine", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, optionsVastane, 0))+1;
-            if (tikud > tikkudeArv) {
-                JOptionPane.showMessageDialog(null, "Alles pole piisavalt tikke. Vali uuesti!", "Väär valik", JOptionPane.ERROR_MESSAGE);
-            } else if (tikud > 3 || tikud < 1) {
-                JOptionPane.showMessageDialog(null, "Vali tikkude arv vahemikust 1-3!", "Väär valik", JOptionPane.ERROR_MESSAGE);
+            Object[] optionsVastane = {"1", "2", "3"}; // nuppude väljad
+            tikud = (JOptionPane.showOptionDialog(null, nimi + ": vali, mitu tikku võtad:",
+                    "tikkude valimine", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, optionsVastane, 0))+1;
+            if (tikud > tikkudeArv) { // kui tikkude valik oli rohkem kui alles on siis GUI näitab seda kasutajale
+                JOptionPane.showMessageDialog(null, "Alles pole piisavalt tikke. Vali uuesti!",
+                        "Väär valik", JOptionPane.ERROR_MESSAGE);
+            } else if (tikud > 3 || tikud < 1) { // kui kuidagi valitakse rohkem kui 3 või vähem kui 1 tikku, siis GUI näitab vastavat kirjet kasutajale
+                JOptionPane.showMessageDialog(null, "Vali tikkude arv vahemikust 1-3!",
+                        "Väär valik", JOptionPane.ERROR_MESSAGE);
             } else break;
         }
         return tikud;
-    }//küsib mängijalt mitu tikku võtab
+    }
 }
--- Rühmatöö/src/MängijaComputerHard.java
+++ Rühmatöö/src/MängijaComputerHard.java
@@ -1,10 +1,10 @@
 public class MängijaComputerHard extends MängijaComputer {
     @Override
-    public int käik(int tikkudeArv) {
-        int b = (tikkudeArv - 1) % 4;
+    public int käik(int tikkudeArv) { // samuti siin overriditakse MängijaCompuuter klassi käigu meetodit
+        int b = (tikkudeArv - 1) % 4; // tikkude arvust jäägi neljaga jagamisel, kuna siis jääb vastasele võtta alati viimane tikk
         if (b == 0) {
             b = 1;
         }
-        return b;
-    }//tikkude arvust jäägi neljaga jagamisel, kuna siis jääb vastasele võtta alati viimane tikk
+        return b; // tagastatakse peameetodile võetud tikkude arv
+    }
 }
